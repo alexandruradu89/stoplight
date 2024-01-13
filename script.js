054daf3fba2019b7b48d74a93b69a610d8ba9361
alexandruradu89 <89183567+alexandruradu89@users.noreply.github.com>
2024-01-13 19:10:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function changeColour() {
-    var colour = document.getElementById("stoplightButton").classList;
+    let colour = document.getElementById("stoplightButton").classList;
     if (colour.contains("btn-danger")) {
         document.getElementById("stoplightButton").className = "btn btn-warning btn-lg";
         return;
-    } 
-    if (colour.contains("btn-warning")) {
+    } else if (colour.contains("btn-warning")) {
         document.getElementById("stoplightButton").className = "btn btn-success btn-lg";
         return;
-    } 
-    if (colour.contains("btn-success")) {
+    } else if (colour.contains("btn-success")) {
         document.getElementById("stoplightButton").className = "btn btn-danger btn-lg";
         return;
     }
